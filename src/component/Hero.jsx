d43da5b4516d27f9d85d00226eaedc819c8ab516
Akashioreki12 <akramtalibi1902@gmail.com>
2023-08-08 14:25:28 +0100

    shapes added

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -4,7 +4,13 @@ import heroPhoto from '../assets/hero-photo.jpeg';
 export default function Hero() {
   return (
     <>
-      <div className="flex flex-wrap m-[8%] lg:h-screen lg:mt-11">
+      <div>
+
+        
+      </div>
+
+
+      <div className="flex flex-wrap m-[8%] lg:h-screen lg:mt-11 realtive">
         <div className="flex items-center w-full lg:w-1/2 mb-6">
           <div className="max-w-2xl mx-8">
             <h1 className="block font-bold leading-snug tracking-tight">
